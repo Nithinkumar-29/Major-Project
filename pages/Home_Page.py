56cc1228ea9216d95f14a1592f39b5ddb71ec3f9
Nithinkumar-29 <105154379+Nithinkumar-29@users.noreply.github.com>
2024-03-31 23:27:57 +0530

    Update Home_Page.py

--- pages/Home_Page.py
+++ pages/Home_Page.py
@@ -173,7 +173,7 @@ if(img is not None):
 	st.image(img2)
 	#img = tf.keras.utils.load_img(word,target_size=(224,224,3))
 	img1 = tf.keras.preprocessing.image.img_to_array(img2)
-	#img1 = np.expand_dims(img2,axis=0)
+	img1 = np.expand_dims(img1,axis=0)
 	model = tf.keras.models.load_model("alexnet_5.h5",compile=False)
 	model.compile(optimizer='adam',loss='categorical_crossentropy')
 	pred = model.predict(img1)
